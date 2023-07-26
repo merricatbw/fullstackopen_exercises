42605273a0d3ec14d5e27d20c25259a6f9ec6372
merricatbw <zkerr4032@outlook.com>
2023-07-26 10:56:02 +0100

    Seperate out components

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -9,19 +9,30 @@ const App = () => {
 
   return (
     <div>
-      <h1>{course}</h1>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+      <Header course={course} />
+      <Content part={part1} exercises={exercises1} />
+      <Content part={part2} exercises={exercises2} />
+      <Content part={part3} exercises={exercises3} />
+      <Total exercises={[exercises1, exercises2, exercises3]} />
     </div>
   )
 }
+
+const Header = ({ course }) => {
+  return <h1>{course}</h1>
+}
+
+const Content = ({ part, exercises }) => {
+  return (
+    <p> {part} {exercises} </p>
+  )
+}
+
+const Total = ({ exercises }) => {
+  return (
+    <p>Number of exercises {exercises.reduce((a, b) => a + b, 0)}</p>
+  )
+}
+
 
 export default App
