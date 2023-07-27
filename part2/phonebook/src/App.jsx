085e0100b6e939d674b29b67d0765db2424cc4a1
merricatbw <zkerr4032@outlook.com>
2023-07-27 14:01:16 +0100

    Add functionality for new contacts

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -6,19 +6,38 @@ const App = () => {
   ]) 
   const [newName, setNewName] = useState('')
 
+  const addPerson = e => {
+    e.preventDefault()
+    setPersons([
+      ...persons,
+      {
+        name: newName,
+      }
+    ])
+    setNewName('')
+  }
+
+  const handleChange = e => {
+    setNewName(e.target.value)
+  }
+
+  const renderNames = persons.map(person => <li key={person.name}>{person.name}</li>)
+
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={addPerson}>
         <div>
-          name: <input />
+          name: <input onChange={handleChange} value={newName} />
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      ...
+      <ul style={{listStyleType: 'none'}}>
+        {renderNames} 
+      </ul>
     </div>
   )
 }
